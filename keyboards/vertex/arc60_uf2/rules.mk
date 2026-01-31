cf58666e493824a86aa712b3b51e6d31d16b23b7
mitsuru <codeberg@yamamech.sh>
2026-01-31 16:31:45 +0000

    keyboards: remove DEFAULT_FOLDER from vertex/arc60_uf2

--- keyboards/vertex/arc60_uf2/rules.mk
+++ keyboards/vertex/arc60_uf2/rules.mk
@@ -12,10 +12,5 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SLEEP_LED_ENABLE = no
 
-
-DEFAULT_FOLDER = vertex/arc60_uf2
-
-
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
